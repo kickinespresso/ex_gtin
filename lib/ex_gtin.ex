f45e0492388527e3beeed68c3bd8fc0813937936
Chris Desch <cdesch@gmail.com>
2017-07-03 16:00:34 -0400

    Adding Majority of functions
    Signed-off-by: Chris Desch <cdesch@gmail.com>

--- lib/ex_gtin.ex
+++ lib/ex_gtin.ex
@@ -1,18 +1,94 @@
 defmodule ExGtin do
   @moduledoc """
-  Documentation for ExGtin.
+  Documentation for ExGtin. This library provides
+  functionality for validating GTIN compliant codes.
   """
-
   @doc """
-  Hello world.
+  Check for valid  GTIN-8, GTIN-12, GTIN-13, GTIN-14, GSIN, SSCC codes
+
+  Returns `{:ok}` or `{:error}`
 
   ## Examples
 
-      iex> ExGtin.hello
-      :world
+      iex> ExGtin.gtin_check_digit("6291041500213")
+      {:ok}
+
+      iex> ExGtin.gtin_check_digit("6291041500214")
+      {:error}
+  """
+  @spec gtin_check_digit(string) :: { atom }
+  def gtin_check_digit(number) when is_bitstring(number) do
+    number
+      |> String.codepoints
+      |> Enum.map(fn(x) -> String.to_integer(x) end)
+      |> gtin_check_digit
+  end
+
+  @spec gtin_check_digit(number) :: { atom }
+  def gtin_check_digit(number) when is_number(number), do: gtin_check_digit(Integer.digits(number))
+
+
+  @spec gtin_check_digit(list(number)) :: { atom }
+  def gtin_check_digit(number) do
+    {code, check_digit} = Enum.split(number, length(number) - 1)
+    calculated_check_digit = code
+      |> multiply_and_sum_array
+      |> subtract_from_nearest_multiple_of_ten
+
+    case calculated_check_digit == Enum.at(check_digit, 0) do
+      true -> {:ok}
+      _ -> {:error}
+    end
+  end
+
+
+  @doc """
+  Calculates the sum of the digits in a string and multiplied value based on index order
+
+  Returns sum of values
+
+  ## Examples
+
+      iex> ExGtin.multiply_and_sum_array([6,2,9,1,0,4,1,5,0,0,2,1])
+      57
 
   """
-  def hello do
-    :world
+  @spec multiply_and_sum_array(list(number)) :: number
+  def multiply_and_sum_array(numbers) do
+    numbers
+      |> Enum.reverse
+      |> Stream.with_index
+      |> Enum.reduce(0, fn({num, idx}, acc) ->
+        acc + (num * mult_by_index_code(idx))
+      end)
   end
+
+
+  @doc """
+  Calculates the difference of the highest rounded multiple of 10
+
+  Returns the difference of the highest rounded multiple of 10
+
+  ## Examples
+
+      iex> ExGtin.subtract_from_nearest_multiple_of_ten(57)
+      3
+
+  """
+  @spec subtract_from_nearest_multiple_of_ten(number) :: number
+  def subtract_from_nearest_multiple_of_ten(number) do
+    mod(10 - mod(number, 10), 10)
+  end
+
+  def mult_by_index_code(index) do
+    case mod(index, 2) do
+      0 -> 3
+      _ -> 1
+    end
+  end
+
+  defp mod(x,y) when x > 0, do: rem(x, y);
+  defp mod(x,y) when x < 0, do: rem(y+x, y);
+  defp mod(0,_y), do: 0
+
 end
--- mix.exs
+++ mix.exs
@@ -5,9 +5,16 @@ defmodule ExGtin.Mixfile do
     [app: :ex_gtin,
      version: "0.1.0",
      elixir: "~> 1.4",
+     description: description(),
+     aliases: aliases(),
+     package: package(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     source_url: "https://github.com/kickinespresso/ex_gtin",
+     test_coverage: [tool: ExCoveralls],
+     coverallspreferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+   ]
   end
 
   # Configuration for the OTP application
@@ -18,16 +25,30 @@ defmodule ExGtin.Mixfile do
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:credo, "~> 0.5", only: [:dev, :test]},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.7", only: :test}
+    ]
   end
+
+  defp package do
+    [
+      maintainers: ["KickinEspresso"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/kickinespresso/ex_gtin"}
+    ]
+  end
+
+  defp aliases do
+    [c: "compile"]
+  end
+
+  defp description do
+     """
+       Elixir GTIN Validation Library.
+     """
+   end
+
 end
